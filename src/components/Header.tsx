57ecd64ce1b4fcff55f9a75ab9d3c4e24b64ec6d
Liew Wen Yu <liewwenyu@outlook.com>
2024-03-06 18:40:17 +0800

    fixed header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,12 @@ export default function Header() {
 
     return (
         <div className="w-screen flex items-center justify-center py-8 text-white text-xl lg:px-[17%] fixed left-0 right-0 z-50 bg-black">
-            <a href="#about" className="lg:block hidden flex-1 text-center hover:text-highlight">About</a>
-            <a href="#sponsors" className="lg:block hidden flex-1 text-center hover:text-highlight">Sponsors</a>
+            <a href="/#about" className="lg:block hidden flex-1 text-center hover:text-highlight">About</a>
+            <a href="/#sponsors" className="lg:block hidden flex-1 text-center hover:text-highlight">Sponsors</a>
             <a href="/" className="lg:block">
                 <img src={Logo} alt="" className="px-10 logo cursor-pointer" />
             </a>
-            <a href="#contacts" className="lg:block hidden flex-1 text-center hover:text-highlight">Contact</a>
+            <a href="/#contacts" className="lg:block hidden flex-1 text-center hover:text-highlight">Contact</a>
             <a href="/details" className="lg:block hidden flex-1 text-center hover:text-highlight">Details</a>
         </div>
     );
