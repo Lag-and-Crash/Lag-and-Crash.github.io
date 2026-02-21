6c38667e5d1bb844da3f19f4357c0131e4cfbf1c
Ravin06 <ravinshappyworld@gmail.com>
2026-02-21 22:52:31 +0800

    Updates

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -49,6 +49,7 @@ const tiers: Tier[] = [
         borderClass: 'border-cosmic-text-muted/15',
         sponsors: [
             { name: 'AISP', src: '/images/sponsors/AISP/AiSP.png', href: 'https://www.aisp.sg/', imgClass: 'w-[180px] h-[120px] object-contain' },
+            { name: 'YBN', src: '/images/sponsors/YBN/YBN.png', href: 'https://www.yes-but-no.org/', imgClass: 'w-[180px] h-[120px] object-contain' }
         ],
     },
 ];
